4154890a74b5ce8154982d34dc6f24428c91407d
leanne63 <leanne63@users.noreply.github.com>
2016-05-07 21:22:15 -0500

    set up storyboard, view controller outlets

--- RegExTester/ViewController.swift
+++ RegExTester/ViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 	
-	// MARK: - Overrides
+	// MARK: - Properties
+	
+	@IBOutlet weak var ignoreCaseSwitch: UISwitch!
+	@IBOutlet weak var globalSwitch: UISwitch!
+	
+	@IBOutlet weak var regexCodeField: UITextField!
+	@IBOutlet weak var testStringField: UITextField!
+	
+	@IBOutlet weak var resultsTextView: UITextView!
+	
+	
+	// MARK: - Life Cycle Overrides
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -22,11 +33,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
 	
 	func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
 		
-		let newText: NSString = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
-		
-		if newText.length > 5 {
-			return false
-		}
+//		let newText: NSString = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
+//		
+//		if newText.length > 5 {
+//			return false
+//		}
 		
 		return true
 	}
